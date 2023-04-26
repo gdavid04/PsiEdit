c978c18bc5c1335875773d5df591d295a32e3cd8
GDavid <gabordavid04@gmail.com>
2023-04-26 18:45:08 +0200

    Safari support

--- snbt.mjs
+++ snbt.mjs
@@ -4,8 +4,9 @@ export function snbt2json(snbt) {
 		return JSON.parse(snbt);
 	} catch (e) {
 		// if that fails, try converting SNBT to JSON
+		// this is a temporary hack until I write a proper SNBT parser
 		return JSON.parse(snbt
-			.replaceAll(/(?<=^[^"]*(?:"[^"]*(?<!\\)"[^"]*)*)\b[^\W\d]\w*/g, '"$&"')
-			.replaceAll(/\b([0-9]+)[bL]/g, '$1'));
+			.replaceAll(/\b[^\W\d][\w\.]*(?=[^"]*(?:"(?:(?:[^"]|\\")*)?"[^"]*)*$)/g, '"$&"')
+			.replaceAll(/\b([0-9]+)[a-zA-Z](?=[^"]*(?:"(?:(?:[^"]|\\")*)?"[^"]*)*$)/g, '$1'));
 	}
 }

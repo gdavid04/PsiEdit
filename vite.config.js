698737351a6cf79792901ec3b5d98483b3066fca
Vonr <dtaclanlegion@gmail.com>
2023-05-11 10:25:29 +0800

    Remove vite config build options and add debug ls

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import topLevelAwait from 'vite-plugin-top-level-await';
 export default defineConfig({
 	base: '/PsiEdit/',
 	root: 'src',
-	build: {
-		outDir: '../dist',
-		emptyOutDir: true
-	},
 	plugins: [ topLevelAwait() ]
 });

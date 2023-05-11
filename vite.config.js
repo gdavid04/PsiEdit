b9d59ce963abc157ffa956640eff0c8eafcbdec4
Vonr <dtaclanlegion@gmail.com>
2023-05-11 10:03:27 +0800

    Add vite base option

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import topLevelAwait from 'vite-plugin-top-level-await';
 
 export default defineConfig({
+	base: '/PsiEdit/',
 	root: 'src',
 	build: {
 		outDir: '..',

b1aa64ed24ce81f21225a67a5ae995c983bdf7ae
GDavid <gabordavid04@gmail.com>
2024-03-14 16:17:44 +0100

    Fix PWA icon

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,9 @@ export default defineConfig({
 		emptyOutDir: true,
 		rollupOptions: {
 			input: Object.assign({
-				main: resolve(__dirname, 'src/index.html')
+				main: resolve(__dirname, 'src/index.html'),
+				manifest: resolve(__dirname, 'src/app.webmanifest'),
+				icon: resolve(__dirname, 'src/icon.png'),
 			}, ['psi', 'phi'].map(name => resolve(__dirname, `src/pieces/${name}.html`)))
 		}
 	}
